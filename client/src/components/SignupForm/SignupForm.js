9404abf4a7feb3c65bd3afc56b19c09fd39fe712
Murad Atta <murad.atta@gmail.com>
2020-10-05 18:26:17 -0400

    adding the security question the signup form

--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -53,7 +53,31 @@ function SignupForm() {
             </InputGroup>
           </Form.Group>
         </Form.Row>
- 
+        <Form.Row>
+          <Form.Group as={Col} md="10" controlId="validationCustom01">
+            <Form.Label>Security Question 1</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Security Question 1"
+              defaultValue="What is the make or model of your first car?"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+        <Form.Group as={Col} md="10" controlId="validationCustom02">
+            <Form.Label>Security Question 2</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Security Question 2"
+              defaultValue="In what City you were born?"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
 
         <Button type="submit">Submit form</Button>
       </Form>
