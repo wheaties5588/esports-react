8c2c115e1878f75616ad8c6988511a9c567727eb
Matt W <matt.wieciech@gmail.com>
2020-10-09 16:15:35 -0400

    trying and failing at connecting the db

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 
-// Post routes
+// User routes
 router.use("/users", userRoutes);
 
 module.exports = router;
