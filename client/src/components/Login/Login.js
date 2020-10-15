0f3f72ff1de3609a5c84db62f9260fe714f868e3
Matt Wieciech <matt.wieciech@gmail.com>
2020-10-14 23:18:23 -0400

    Got pandascore api data to render to page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -29,7 +29,7 @@ function Login() {
     }
     API.getUserEmail(userInfo).then(result =>{
       console.log("I have recieved the data from the data base")
-      console.log(result.data[0].email)
+      console.log(result)
       let Email = email.toLowerCase();
       if (Email === result.data[0].email.toLowerCase() && password === result.data[0].password) {
         console.log("The email and password are in the database")
--- client/src/components/TwitchCard/TwitchCard.js
+++ client/src/components/TwitchCard/TwitchCard.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import psAPI from '../../utils/pandaScoreApi';
 
 
 function TwitchCard() {
     
-    // const psTournydata = await psAPI.getTournament("/dota2/tournaments", 10).then( data => {return data})
+    const [psData, setPsData] = useState([]);
+
+    useEffect(() => {
+        loadPsData() 
+    }, [])
+    
+    function loadPsData() {
+        psAPI.getTournament("/dota2/tournaments", 10)
+        .then( data => {
+            console.log(data);
+            setPsData(data.data);
+        });
+    }
+    
+    
 
     
-    // console.log(psTournydata);
     
     
     
     return (
-        <div id="dotaTournamentDiv">
-            {/* {psTournydata.map( (el) => (
-                <div className="image-type">{el.slug}</div> 
+        <div>
+            
+            <div id="dotaTournamentDiv">
+            {psData.map( (el, index) => (
+                <div className="tourney-card" key={index}>{el.slug}</div> 
             )
-            )} */}
+            )}
         </div>
+        
+        <div className="column">
+            <div className="dropdown">
+                <div className="dropdown-trigger">
+                    <button className="button"  aria-haspopup="true" aria-controls="dropdown-menu">
+                    <span>Select a Series</span>
+                    <span className="icon is-small">
+                        <i className="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                    </button>
+                </div>
+                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                    <div id="dropdownContent" class="dropdown-content">
+                        {psData.map( (el, index) => (
+                            <div className="dropdown-item" key={index}>{el.slug}</div> 
+                        )
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        </div>
+        
     )
 }
 
