a5d006f236a9d9091f8c1f10c5dc47dafaf16cc0
Matt Wieciech <matt.wieciech@gmail.com>
2020-10-22 20:26:19 -0400

    App restructure with new login and JWT

---
+++ esports-react/src/utils/TwitchApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+var twitchKey = "5im67pxtrmahhr4u8f270ntw5l5srp";
+        // var twitchStreams =
+        //   "https://cors-anywhere.herokuapp.com/https://api.twitch.tv/kraken/streams/?game=" +
+        //   twitchQuery +
+        //   "&limit=" +
+        //   twitchLimit +
+        //   "&api_version=5&language=en";
+
+export default {
+  // Gets all users
+  getStream: function (twitchQuery, twitchLimit) {
+    return axios.get(
+      "https://cors-anywhere.herokuapp.com/https://api.twitch.tv/kraken/streams/?game=" +
+        twitchQuery +
+        "&limit=" +
+        twitchLimit +
+        "&api_version=5&language=en="
+    ),
+    {
+        headers: {
+        "Client-ID": twitchKey
+    }}
+  }
+};
